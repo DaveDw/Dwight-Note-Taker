67ed1164850e8720f41b3c42ca953b612bda5616
DaveDw <93271098+DaveDw@users.noreply.github.com>
2022-08-31 13:55:43 -0400

    changes to package json

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get('/notes', (req, res) =>
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
